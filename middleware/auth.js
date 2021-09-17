eaa38496e7f883590bf68afedfbe32cb3637bc84
jwright90 <jason.wright90@gmail.com>
2021-09-17 21:00:42 +0100

    final updates

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,9 @@
+const dotenv = require('dotenv');
 const jwt = require('jsonwebtoken');
-const config = require('config');
+
+dotenv.config();
+
+const jwtSecret = process.env.JWT_SECRET;
 
 module.exports = function (req, res, next) {
   const token = req.header('x-auth-token');
@@ -9,7 +13,7 @@ module.exports = function (req, res, next) {
   }
 
   try {
-    const decoded = jwt.verify(token, config.get('jwtSecret'));
+    const decoded = jwt.verify(token, jwtSecret);
     req.user = decoded.user;
     next();
   } catch (err) {
